f56d87edea7a1be5683e588ea71319b99dc754e4
ariroseonline <ariroseonline@gmail.com>
2016-01-24 18:11:07 -0500

    better lazy load

--- dist/app.js
+++ dist/app.js
@@ -7,10 +7,32 @@ $.bridget('flickity', Flickity);
 
 $(document).ready(function(){
 
-var lazyLoadImages = function(projectId) {
-	$('.projects').find('[data-project-id=' + projectId + '] img').reveal("fadeIn", 5000);
-	//fadeIn doesn't work...maybe http://stackoverflow.com/questions/3818063/dont-load-hidden-images
-}
+	var lazyLoadImages = function(projectId) {
+		var $projectImages = $('.projects').find('[data-project-id=' + projectId + '] img');
+		var $nextImages = $('.projects').find('[data-project-id=' + (projectId+1) + '] img');
+
+		//add in next project images for good measure
+		$images = $.merge($projectImages, $nextImages);
+
+		var loadImagesInOrder = function(counter) {
+			//Break out if no more images
+			 if(counter== $images.length) { return; }
+
+			 //Grab an image obj
+			 var image = $images[counter];
+
+			 //Monitor load or error events, moving on to next image in either case
+			 image.onload = image.onerror = function() { loadImagesInOrder(counter+1); }
+
+			 //Change source (then wait for event)
+			 image.src = $(image).data('src');
+		}
+
+		loadImagesInOrder(0);
+
+	}
+
+	
 
 	//Don't load images until project clicked on 
 	$("img").not(":visible").each(function () {
@@ -30,7 +52,7 @@ var lazyLoadImages = function(projectId) {
 	var $firstProject = $projects.first()
 
 	$firstProject.removeClass('hide');
-	lazyLoadImages(1); 
+	lazyLoadImages(0); 
 
 	$firstProject.find('.project-images').flickity({
 	  // options
@@ -70,31 +92,31 @@ var lazyLoadImages = function(projectId) {
 
 
 
-(function($){
-    // Bind the function to global jQuery object.
-    $.fn.reveal = function(){
-        // Arguments is a variable which is available within all functions
-        // and returns an object which holds the arguments passed.
-        // It is not really an array, so by calling Array.prototype
-        // he is actually casting it into an array.
-        var args = Array.prototype.slice.call(arguments);
+// (function($){
+//     // Bind the function to global jQuery object.
+//     $.fn.reveal = function(){
+//         // Arguments is a variable which is available within all functions
+//         // and returns an object which holds the arguments passed.
+//         // It is not really an array, so by calling Array.prototype
+//         // he is actually casting it into an array.
+//         var args = Array.prototype.slice.call(arguments);
 
-        // For each elements that matches the selector:
-        return this.each(function(){
-            // this is the dom element, so encapsulate the element with jQuery.
-            var img = $(this),
-                src = img.data("src");
+//         // For each elements that matches the selector:
+//         return this.each(function(){
+//             // this is the dom element, so encapsulate the element with jQuery.
+//             var img = $(this),
+//                 src = img.data("src");
 
-            // If there is a data-src attribute, set the attribute
-            // src to make load the image and bind an onload event.
-            src && img.attr("src", src).load(function(){
-                // Call the first argument passed (like fadeIn, slideIn, default is 'show').
-                // This piece is like doing img.fadeIn(1000) but in a more dynamic way.
-                img[args[0]||"show"].apply(img, args.splice(1));
-            });
-        });
-    }
-}($));
+//             // If there is a data-src attribute, set the attribute
+//             // src to make load the image and bind an onload event.
+//             src && img.attr("src", src).load(function(){
+//                 // Call the first argument passed (like fadeIn, slideIn, default is 'show').
+//                 // This piece is like doing img.fadeIn(1000) but in a more dynamic way.
+//                 img[args[0]||"show"].apply(img, args.splice(1));
+//             });
+//         });
+//     }
+// }($));
 
 
 
