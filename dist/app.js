6db022bd11e9a22bd97227523c58ff01dbf05fa8
ariroseonline <ariroseonline@gmail.com>
2016-01-24 17:38:02 -0500

    implemented lazyload

--- dist/app.js
+++ dist/app.js
@@ -6,11 +6,32 @@ require('jquery-bridget');
 $.bridget('flickity', Flickity);
 
 $(document).ready(function(){
+
+var lazyLoadImages = function(projectId) {
+	$('.projects').find('[data-project-id=' + projectId + '] img').reveal("fadeIn", 5000);
+	//fadeIn doesn't work...maybe http://stackoverflow.com/questions/3818063/dont-load-hidden-images
+}
+
+	//Don't load images until project clicked on 
+	$("img").not(":visible").each(function () {
+    $(this).data("src", this.src);
+    this.src = "";
+  });
+
+	//lazy load
+	// var reveal = function (selector) {
+	// 	debugger
+	//   var img = $(selector);
+	//   img[0].src = img.data("src");
+	// }
+
 	var $projectsNav = $('.projects-nav');
 	var $projects = $('.project');
 	var $firstProject = $projects.first()
 
 	$firstProject.removeClass('hide');
+	lazyLoadImages(1); 
+
 	$firstProject.find('.project-images').flickity({
 	  // options
 	  imagesLoaded: true,
@@ -27,20 +48,11 @@ $(document).ready(function(){
 			var projectId = $(this).data('project-id');
 			var $project = $('.projects').find('[data-project-id=' + projectId + ']');
 			$project.removeClass('hide');
-
-			// $project.fadeIn(200, function() {
-			// 	// setTimeout(function(){
-			// 		console.log('yo')
-			// 		$('.project-images').flickity({
-			// 					  // options
-			// 					  imagesLoaded: true,
-			// 					  wrapAround: true,
-			// 					  pageDots: false
-			// 					  // setGallerySize: true
-			// 					});
-			// 	// },2000);
-			// });
-
+			
+			//load in images lazily
+			lazyLoadImages(projectId);
+			// lazyLoadImages(projectId + 1); //the next project, foreseeing their next click :)
+			
 			//initiate flickity on it
 			$project.find('.project-images').flickity({
 			  // options
@@ -51,35 +63,38 @@ $(document).ready(function(){
 			});
 	
 	})
-	// var flkty = $projects.data('flickity');
-
-	// // update selected projectLinks
-	// flkty.on('cellSelect', function() {
-	//   $projectLinks.filter('.is-selected')
-	//     .removeClass('is-selected');
-	//   $projectLinks.eq( flkty.selectedIndex )
-	//     .addClass('is-selected');
-	// });
-
-	// flkty.select(0);
-
-	// // select cell on button click
-	// $projectsNav.on( 'click', '.project-link', function() {
-	//   var index = $(this).index();
-	//   $projects.flickity( 'select', index );
-	// });
-	// // previous
-	// $('.button--previous').on( 'click', function() {
-	//   $projects.flickity('previous');
-	// });
-	// // next
-	// $('.button--next').on( 'click', function() {
-	//   $projects.flickity('next');
-	// });
 
 
 
 });
+
+
+
+(function($){
+    // Bind the function to global jQuery object.
+    $.fn.reveal = function(){
+        // Arguments is a variable which is available within all functions
+        // and returns an object which holds the arguments passed.
+        // It is not really an array, so by calling Array.prototype
+        // he is actually casting it into an array.
+        var args = Array.prototype.slice.call(arguments);
+
+        // For each elements that matches the selector:
+        return this.each(function(){
+            // this is the dom element, so encapsulate the element with jQuery.
+            var img = $(this),
+                src = img.data("src");
+
+            // If there is a data-src attribute, set the attribute
+            // src to make load the image and bind an onload event.
+            src && img.attr("src", src).load(function(){
+                // Call the first argument passed (like fadeIn, slideIn, default is 'show').
+                // This piece is like doing img.fadeIn(1000) but in a more dynamic way.
+                img[args[0]||"show"].apply(img, args.splice(1));
+            });
+        });
+    }
+}($));
 
 
 
